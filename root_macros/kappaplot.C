aa85a61d7fe992b5a0b5272b44c67bd5130e054a
TCThenna <hendrik.jansen@desy.de>
2016-03-31 16:31:00 +0200

    Last update of pics

--- root_macros/kappaplot.C
+++ root_macros/kappaplot.C
@@ -22,17 +22,18 @@
   gre->SetTitle("");
   gre->SetMarkerStyle(24);
   gre->SetMarkerSize(1.3);
-
-  gre->SetPoint(0, 6, 0.860);
-  gre->SetPointError(0, 6*0.05, 0.0324);
-  gre->SetPoint(1, 5, 0.854);
-  gre->SetPointError(1, 5*0.05, 0.0335);
-  gre->SetPoint(2, 4, 0.851);
-  gre->SetPointError(2, 4*0.05, 0.0319);
-  gre->SetPoint(3, 3, 0.857);
-  gre->SetPointError(3, 3*0.05, 0.0259);
-  gre->SetPoint(4, 2, 0.851);
-  gre->SetPointError(4, 2*0.05, 0.0197);
+  
+  // biased
+  gre->SetPoint(0, 6, 0.755);
+  gre->SetPointError(0, 6*0.05, 0.041);
+  gre->SetPoint(1, 5, 0.751);
+  gre->SetPointError(1, 5*0.05, 0.035);
+  gre->SetPoint(2, 4, 0.751);
+  gre->SetPointError(2, 4*0.05, 0.03);
+  gre->SetPoint(3, 3, 0.755);
+  gre->SetPointError(3, 3*0.05, 0.025);
+  gre->SetPoint(4, 2, 0.751);
+  gre->SetPointError(4, 2*0.05, 0.023);
 
   mg->Add(gre,"p");
 
@@ -41,16 +42,17 @@
   gre2->SetTitle("");
   gre2->SetMarkerStyle(20);
   gre2->SetMarkerSize(1.3);
-
-  gre2->SetPoint(0, 6, 1.064);
+  
+  // unbiased
+  gre2->SetPoint(0, 6, 0.937);
   gre2->SetPointError(0, 6*0.05, 0.0235);
-  gre2->SetPoint(1, 5, 1.034);
+  gre2->SetPoint(1, 5, 0.914);
   gre2->SetPointError(1, 5*0.05, 0.0206);
-  gre2->SetPoint(2, 4, 1.023);
+  gre2->SetPoint(2, 4, 0.903);
   gre2->SetPointError(2, 4*0.05, 0.0156);
-  gre2->SetPoint(3, 3, 1.016);
+  gre2->SetPoint(3, 3, 0.895);
   gre2->SetPointError(3, 3*0.05, 0.0141);
-  gre2->SetPoint(4, 2, 0.991);
+  gre2->SetPoint(4, 2, 0.872);
   gre2->SetPointError(4, 2*0.05, 0.0119);
 
   mg->Add(gre2,"p");
@@ -67,13 +69,21 @@
   mg->GetYaxis()->SetTitleOffset(1.34);
 
   TF1* f1 = new TF1("f1","[0]", 1.65, 6.35);
-  f1->SetParameter(0,0.854);
+  f1->SetParameter(0,0.7525); // uncert 0.0111
   f1->SetLineStyle(2);
   f1->SetLineWidth(2);
 
   f1->Draw("same");
 
-  TLegend *leg = new TLegend(0.25,0.25,0.55,0.42,NULL,"brNDC");
+  TF1* f2 = new TF1("f2","[0]+x*[1]", 1.65, 6.35);
+  f2->SetParameter(0,0.8447); // uncert 0.0111
+  f2->SetParameter(1,1.497e-2); // uncert 0.0111
+  f2->SetLineStyle(9);
+  f2->SetLineWidth(2);
+
+  f2->Draw("same");
+
+  TLegend *leg = new TLegend(0.25,0.24,0.55,0.48,NULL,"brNDC");
   leg->SetBorderSize(0);
   leg->SetTextSize(0.03);
   leg->SetFillStyle(1001);
@@ -83,7 +93,9 @@
   leg->SetHeader("#kappa for #xi_{n} = 6");
   leg->AddEntry(gre2,"unbiased tracks","p");
   leg->AddEntry(gre, "biased tracks","p");
-  leg->AddEntry(f1,"const. fit: c = 0.854 #pm 0.012","l");
+  leg->AddEntry(f1,"const. fit: c = 0.753 #pm 0.013","l");
+  leg->AddEntry(f2,"linear fit: y_{0} = 0.845 #pm 0.020,","l");
+  leg->AddEntry(f2,"                 m = 1.5e-2 #pm 0.5e-2","");
   leg->Draw();
   
 
--- root_macros/sigmaint.C
+++ root_macros/sigmaint.C
@@ -23,11 +23,12 @@
   gre->SetMarkerStyle(20);
   gre->SetMarkerSize(1.3);
 
-  gre->SetPoint(0, 6, 3.263);
-  gre->SetPoint(1, 5, 3.297);
-  gre->SetPoint(2, 4, 3.306);
-  gre->SetPoint(3, 3, 3.313);
-  gre->SetPoint(4, 2, 3.357);
+  // unbiased
+  gre->SetPoint(0, 6, 3.269);
+  gre->SetPoint(1, 5, 3.292);
+  gre->SetPoint(2, 4, 3.313);
+  gre->SetPoint(3, 3, 3.331);
+  gre->SetPoint(4, 2, 3.385);
   gre->SetPointError(0, 6*0.05, 0.059);
   gre->SetPointError(1, 5*0.05, 0.071);
   gre->SetPointError(2, 4*0.05, 0.083);
@@ -42,16 +43,17 @@
   gre2->SetMarkerStyle(24);
   gre2->SetMarkerSize(1.3);
 
-  gre2->SetPoint(0, 6, 3.236);
-  gre2->SetPoint(1, 5, 3.260);
-  gre2->SetPoint(2, 4, 3.291);
-  gre2->SetPoint(3, 3, 3.325);
-  gre2->SetPoint(4, 2, 3.395);
+  // biased
+  gre2->SetPoint(0, 6, 3.241);
+  gre2->SetPoint(1, 5, 3.265);
+  gre2->SetPoint(2, 4, 3.298);
+  gre2->SetPoint(3, 3, 3.337);
+  gre2->SetPoint(4, 2, 3.415);
   gre2->SetPointError(0, 6*0.05, 0.084);
-  gre2->SetPointError(1, 5*0.05, 0.093);
-  gre2->SetPointError(2, 4*0.05, 0.102);
-  gre2->SetPointError(3, 3*0.05, 0.112);
-  gre2->SetPointError(4, 2*0.05, 0.126);
+  gre2->SetPointError(1, 5*0.05, 0.094);
+  gre2->SetPointError(2, 4*0.05, 0.104);
+  gre2->SetPointError(3, 3*0.05, 0.115);
+  gre2->SetPointError(4, 2*0.05, 0.130);
 
   mg->Add(gre2,"p");
 
